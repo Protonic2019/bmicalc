678ccb47784b87724c0d530e97ea991916e004b9
Protonic2019 <sander.nobel@protonmail.com>
2019-09-26 22:29:33 +0200

    1 file modified

--- index.js
+++ index.js
@@ -2,5 +2,27 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var weight = prompt("What is your weight in kilograms?");
+var height = prompt("What is your height in meters?");
+
+function bmiCalc(weight, height){
+  var bmi = weight / (height * height);
+  bmi = Math.round(bmi);
+
+  if (bmi <18.5){
+alert("Your BMI is " + bmi + ", so you are underweight");
+}
+
+  if (bmi >= 18.5 && bmi <= 24.9){
+alert("Your BMI is " + bmi + ", so you have a normal weight");
+}
+
+  if (bmi >24.9){
+alert("Your BMI is " + bmi + ", so you are overweight");
+}
+
+
+}
+
+var bmi = bmiCalc(weight, height);
